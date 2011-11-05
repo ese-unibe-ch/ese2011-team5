7f92f5c9cafaa4f23973db484715bf2ced09c2d6
lyriael <lyriael@gmx.net>
2011-11-05 11:39:24 +0100

    Test class ESEGroup
    A very very short test.
    	new file:   test/modelTests/ESEGroupTests.java
    	new file:   test/modelTests/ESEUserTests.java

---
+++ test/modelTests/ESEGroupTests.java
@@ -0,0 +1,35 @@
+package modelTests;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import models.ESECalendar;
+import models.ESEEvent;
+import models.ESEGroup;
+import models.ESEUser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.UnitTest;
+
+public class ESEGroupTests extends UnitTest{
+	
+	ESEGroup group1;
+	ESEUser dummyOwner = new ESEUser(0, "dummy", "pw", "firstName", "familyName");
+	
+	
+	@Before
+	public void setUp(){
+		group1 = new ESEGroup(0, "Group 1", dummyOwner);
+	}
+	
+	@Test
+	public void shouldInitialize(){
+		assertTrue(group1 != null);
+		assertEquals(0, group1.getGroupID());
+		assertEquals("Group 1", group1.getGroupName());
+		assertEquals(dummyOwner, group1.getOwner());
+	}
+
+}
---
+++ test/modelTests/ESEUserTests.java
@@ -0,0 +1,46 @@
+package modelTests;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import models.ESECalendar;
+import models.ESEEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.UnitTest;
+
+public class ESEUserTests extends UnitTest{
+	
+	//TODO
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void shouldInitialize(){
+		
+	}
+	
+	@Test
+	public void shouldAddCalendar(){
+		
+	}
+	
+	@Test
+	public void shouldAddGroup(){
+		
+	}
+	@Test
+	public void shouldGetAllEvents(){
+		
+	}
+	@Test
+	public void shouldGetAllPublicEvents(){
+		
+	}
+	
+}
